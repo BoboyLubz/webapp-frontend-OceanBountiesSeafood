034720844fc97c4cbb34a76540fce0972f6887fa
BoboyLubz <janrelle.lubiano@carsu.edu.ph>
2022-11-17 14:44:11 +0800

    modified adminPage

--- resources/views/adminPage.blade.php
+++ resources/views/adminPage.blade.php
@@ -104,6 +104,7 @@
                         <ol class="breadcrumb mb-4">
                             <li class="breadcrumb-item active">Dashboard</li>
                         </ol>
+                        <!--
                         <div class="row">
                             <div class="col-xl-3 col-md-6">
                                 <div class="card bg-primary text-white mb-4">
@@ -141,7 +142,8 @@
                                     </div>
                                 </div>
                             </div>
-                        </div>
+                        </div> -->
+                        <!--
                         <div class="row">
                             <div class="col-xl-6">
                                 <div class="card mb-4">
@@ -161,7 +163,7 @@
                                     <div class="card-body"><canvas id="myBarChart" width="100%" height="40"></canvas></div>
                                 </div>
                             </div>
-                        </div>
+                        </div> -->
                         <div class="card mb-4">
                             <div class="card-header">
                                 <i class="fas fa-table me-1"></i>
