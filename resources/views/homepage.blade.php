dfcae39f2d845dd9eacac01217d2be1e5068f1dc
BoboyLubz <janrelle.lubiano@carsu.edu.ph>
2023-01-12 14:07:21 +0800

    add vue js frontend into laravel

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -6,7 +6,8 @@
         <!-- Navigation-->
         <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
             <div class="container px-4 px-lg-5">
-                <a class="navbar-brand" href="#page-top">Ocean Bounties Seafood Market & Restaurant</a>
+                <!--<a class="navbar-brand" href="#page-top">Ocean Bounties Seafood Market & Restaurant</a>-->
+                <img src=assets/img/portfolio/thumbnails/logo.png>
                 <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse" id="navbarResponsive">
                     <ul class="navbar-nav ms-auto my-2 my-lg-0">
@@ -14,8 +15,10 @@
                         <li class="nav-item"><a class="nav-link" href="#services">SERVICES</a></li>
                         <li class="nav-item"><a class="nav-link" href="#portfolio">ANNOUNCEMENT</a></li>
                         <li class="nav-item"><a class="nav-link" href="#contact">CONTACT</a></li>
-                        <li class="nav-item"><a class="nav-link" href="/login">SIGN IN</a></li>
-                    </ul>
+                        <li class="nav-item"><a class="nav-link" href="#contact">MISSION & VISION</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#contact">MAP</a></li>
+                        <li class="nav-item"><a class="nav-link" href="/login">SIGN IN</a></li>   
+                    </ul>     
                 </div>
             </div>
         </nav>
